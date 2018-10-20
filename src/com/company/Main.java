25ffcd53bd998ed98e2645437d1c77f5ba00d940
Solomon West <swest599@gmail.com>
2018-10-20 15:19:09 -0400

    initial commit for javatutorial1

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        double mayan = 2.00;
+        int mayan0 = 10;
+        System.out.println(mayan0 / mayan);
+
+
+    }
+}
+
